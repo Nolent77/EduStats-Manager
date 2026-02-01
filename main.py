3be76273f14e59dabb7c714e4fa8819615fc4dc5
Nolent77 <nolhan795@gmail.com>
2026-02-01 05:25:17 +0100

    updgrade make_student_list_note function, -1 condition to stop the process

--- main.py
+++ main.py
@@ -1,36 +1,39 @@
 # Pour executer cette tâche executer la ligne de commande suivante : py main.py
 
 def make_student_list_note():
-    Note_Student = 0
-    Nb_Student = 0
-    Note_Liste = []
+    note_list = []
 
-    # demande à taper le nombre d'élève
+    # Pour enregistré le nombre d'élève
     while True :
         try:
-            Nb_Student = int(input("Combien d'élève : "))
+            nb_student = int(input("Combien d'élève : "))
 
-            if Nb_Student < 1 :
+            if nb_student < 1 :
                 print("il faut au moin un élève")
             else :
                 break
         except ValueError :
             print(ValueError)
 
-    print(Nb_Student)
-    for i in range(Nb_Student):
-        while True :
-            try :
-                Note_Student = float(input("La note de l'élève " + str(i + 1) +  " est : "))
-                if Note_Student > 0 :
-                    Note_Liste.append(Note_Student)
-                    print("j'ai ajouter la nb à la liste")
-                    break
-                else:
-                    print("la note de l'élève ne peux pas être négative")
-            except ValueError :
-                print("Taper uniquement un nombre")
-    return Note_Liste
+    # Pour enregistré les notes d'élève suivant les nb_student
+    note_student = 0
+    for i in range(nb_student):
+        if note_student != -1 :
+            while True :
+                try :
+                    note_student = float(input("La note de l'élève " + str(i + 1) +  " est : "))
+                    if note_student > 0 :
+                        note_list.append(note_student)
+                        print("j'ai ajouter la nb à la liste")
+                        break
+                    elif note_student == -1:
+                        print("Processus stopper")
+                        break
+                    else:
+                        print("la note de l'élève ne peux pas être négative")
+                except ValueError :
+                    print("Taper uniquement un nombre")
+    return note_list
 
 print(make_student_list_note())
 
